1ad7be8863702ae241293ce059f265dc0ae380e3
Prashant <prashantyadav09783@gmail.com>
2024-04-07 00:30:24 +0530

    added

--- components/AIImage.tsx
+++ components/AIImage.tsx
@@ -8,6 +8,7 @@ import Heading from "./Heading";
 import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { robot } from "../assets";
+import { Web3Button } from "@thirdweb-dev/react";
 
 function AIImage() {
   const fileRef = useRef(null);
@@ -32,6 +33,13 @@ function AIImage() {
     <div className="flex pt-32 mx-12">
     <div className="w-[300%]">
       <h1 className="h1"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ducimus esse quo aliquid soluta fugiat! Impedit necessitatibus quisquam, dicta dolor et ea adipisci vel pariatur cum voluptates, exercitationem alias! Voluptatem.</h1>
+      <div>
+        <Web3Button 
+          
+        >
+          Collect Token
+        </Web3Button>
+      </div>
     </div>
 <div className="w-full">
 <Section>
