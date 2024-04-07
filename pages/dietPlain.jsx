9131300c08106f1abbc5746a475a4f6a70dc18cf
Prashant <prashantyadav09783@gmail.com>
2024-04-07 05:53:12 +0530

    deplo

---
+++ pages/dietPlain.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function dietPlain() {
+  const [formData, setFormData] = useState({
+    weigth: 0,
+    target_calories: 0,
+  });
+  return (
+    <>
+     <form action="">
+      </form> 
+    </>
+  )
+}
+
+export default dietPlain
